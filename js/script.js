9558f6a37aa19e26964db7aa9e4ac53d75abdb1a
RedMadda <maddalenarossaro91@gmail.com>
2021-05-26 15:35:07 +0200

    base solution

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
  
-// Chiedi all’utente il suo nome, poi chiedi il suo cognome, poi chiedi il suo colore preferito
+// Chiedi all’utente il suo nome, poi chiedi il suo cognome, poi chiedi
+//  il suo colore preferito
 // Infine scrivi sulla pagina nomecognomecolorepreferito21
-// Buon divertimento :party_wizard:
-// Bonus: chi vuole può cercare funzioni per trasformare in minuscolo tutta la password generata, nel caso in cui ad esempio  l'utente scriva il proprio nome, cognome e/o colore in stampatello o misto (pIPPO o PIPPO, ecc..
+
+// Bonus: chi vuole può cercare funzioni per trasformare in minuscolo tutta
+//  la password generata, nel caso in cui ad esempio  l'utente scriva il proprio
+//   nome, cognome e/o colore in stampatello o misto (pIPPO o PIPPO, ecc..
+
+// nome
+var nome = prompt("Buongiorno! Inserisci il tuo nome.");
+var cognome = prompt("Bene! Ora inserisci il tuo cognome.");
+var colore = prompt("Bene! Ora, inserisci il tuo colore preferito");
+
+document.getElementById("password").innerHTML = "La tua password è:&nbsp;" + nome + cognome + colore;
