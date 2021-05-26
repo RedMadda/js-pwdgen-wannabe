51d714f39c1d415c69ca636c687a44257042a388
RedMadda <maddalenarossaro91@gmail.com>
2021-05-26 16:25:30 +0200

    bonus

--- js/script.js
+++ js/script.js
@@ -1,15 +1,6 @@
- 
-// Chiedi all’utente il suo nome, poi chiedi il suo cognome, poi chiedi
-//  il suo colore preferito
-// Infine scrivi sulla pagina nomecognomecolorepreferito21
 
-// Bonus: chi vuole può cercare funzioni per trasformare in minuscolo tutta
-//  la password generata, nel caso in cui ad esempio  l'utente scriva il proprio
-//   nome, cognome e/o colore in stampatello o misto (pIPPO o PIPPO, ecc..
-
-// nome
 var nome = prompt("Buongiorno! Inserisci il tuo nome.");
 var cognome = prompt("Bene! Ora inserisci il tuo cognome.");
 var colore =  prompt("Bene! Ora, inserisci il tuo colore preferito");
 var passWord = nome + cognome + colore;
-document.getElementById("password").innerHTML = "La tua password è:&nbsp;" + passWord.toLowerCase();
+document.getElementById("password").innerHTML = "La tua password è:&nbsp;" + "<br/>" + passWord.toLowerCase();
