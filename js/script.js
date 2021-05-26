4efaf543af8c6e17c20559658ca81f5e7fbd9424
RedMadda <maddalenarossaro91@gmail.com>
2021-05-26 15:23:55 +0200

    init

---
+++ js/script.js
@@ -0,0 +1,5 @@
+ 
+// Chiedi all’utente il suo nome, poi chiedi il suo cognome, poi chiedi il suo colore preferito
+// Infine scrivi sulla pagina nomecognomecolorepreferito21
+// Buon divertimento :party_wizard:
+// Bonus: chi vuole può cercare funzioni per trasformare in minuscolo tutta la password generata, nel caso in cui ad esempio  l'utente scriva il proprio nome, cognome e/o colore in stampatello o misto (pIPPO o PIPPO, ecc..
