5ae2f98e244eed52ea0dc828b3b7d4683e1df3d9
RedMadda <maddalenarossaro91@gmail.com>
2021-05-26 16:30:34 +0200

    bonus

--- js/script.js
+++ js/script.js
@@ -3,4 +3,4 @@ var nome = prompt("Buongiorno! Inserisci il tuo nome.");
 var cognome = prompt("Bene! Ora inserisci il tuo cognome.");
 var colore =  prompt("Bene! Ora, inserisci il tuo colore preferito");
 var passWord = nome + cognome + colore;
-document.getElementById("password").innerHTML = "La tua password è:&nbsp;" + "<br/>" + passWord.toLowerCase();
+document.getElementById("password").innerHTML = "La tua password &egrave;:&nbsp;" + "<br/>" + passWord.toLowerCase();
